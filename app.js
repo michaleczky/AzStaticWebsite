bac4ae244e2d078cc9a62bce88c76317ad1c3829
Peter Michaleczky <peter.michaleczky@itron.com>
2020-04-25 09:38:05 +0200

    add logger

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const getenv = require('getenv');
 const path = require('path');
+const winston = require('winston');
 
 const config = {
-    port: getenv('HTTP_PORT', 3000)
+    port: getenv('HTTP_PORT', 3000),
+    loglevel: getenv('LOGLEVEL', 'info')
 };
 
+const logger = winston.createLogger({
+    level: config.loglevel,
+    format: winston.format.simple(),
+    transports: [ new winston.transports.Console() ]
+});
 
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, 'content')))
 
-app.listen(config.port, () => console.log(`App listening on port ${config.port}`));
-
+app.listen(config.port, () => logger.info(`App listening on port ${config.port}`));
